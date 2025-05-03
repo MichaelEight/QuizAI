744fbb459b1ad5ff8c42bc719d9975c908a00445
Eight <werykintosz@wp.eu>
2025-05-03 12:04:34 +0200

    feat: Add basic settings page

--- Frontend/src/SettingsPage.tsx
+++ Frontend/src/SettingsPage.tsx
@@ -9,24 +9,60 @@ export default function SettingsPage({ settings, setSettings }) {
     }));
   };
 
-  // Assign values to each input
-
   return (
-    <>
-      <p>Settings Page</p>
-      <div className="input-container">
-        <p>Closed questions</p>
-        <input
-          type="number"
-          value={settings.amountOfClosedQuestions}
-          min={0}
-          max={10}
-          step={1}
-          onChange={(e) =>
-            handleChange("amountOfClosedQuestions", Number(e.target.value))
-          }
-        />
+    <div className="settings-page-container">
+      <h2>Settings Page</h2>
+      <div className="inputs-container">
+        <div className="input-container">
+          <p>Closed questions</p>
+          <input
+            type="number"
+            min={0}
+            max={10}
+            step={1}
+            value={settings.amountOfClosedQuestions}
+            onChange={(e) =>
+              handleChange("amountOfClosedQuestions", Number(e.target.value))
+            }
+          />
+        </div>
+
+        <div className="input-container">
+          <p>Open questions</p>
+          <input
+            type="number"
+            min={0}
+            max={10}
+            step={1}
+            value={settings.amountOfOpenQuestions}
+            onChange={(e) =>
+              handleChange("amountOfOpenQuestions", Number(e.target.value))
+            }
+          />
+        </div>
+
+        <div className="input-container">
+          <p>Allow multiple correct answers</p>
+          <input
+            type="checkbox"
+            checked={settings.allowMultipleCorrectAnswers}
+            onChange={(e) =>
+              handleChange("allowMultipleCorrectAnswers", e.target.checked)
+            }
+          />
+        </div>
+
+        <div className="input-container">
+          <p>Force multiple correct answers</p>
+          <input
+            type="checkbox"
+            checked={settings.forceMultipleCorrectAnswers}
+            onChange={(e) =>
+              handleChange("forceMultipleCorrectAnswers", e.target.checked)
+            }
+          />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
