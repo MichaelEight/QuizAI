e6a177b49354c9ec079dd726a8f97e99526597bc
quizai <werykintosz@wp.eu>
2026-06-07 15:53:49 +0200

    style: use clear flame icon for streak stat

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -73,7 +73,7 @@ export default function Homepage({ setTasks, setSourceText }: HomepageProps) {
             value={`${userStats.currentStreak}`}
             tone="amber"
             icon={
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.8} d="M12 3c.5 3-1.5 4.5-3 6.5S7 14 9 15c.4-1 1.2-1.8 2-2.5.3 2 1.7 2.8 2.4 4.3.8 1.7-.2 4.2-2.4 4.2C7.6 21 5 18.4 5 14.8 5 10 9.5 8.5 12 3Z" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.8} d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z" />
             }
           />
           <StatCard
